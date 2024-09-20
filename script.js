fce388baffe899d5819cef40c6ca129fe9986a26
Antimekumawat111 <aktank810@gmail.com>
2024-09-20 10:23:39 +0530

    edit deleteFunction

--- script.js
+++ script.js
@@ -11,16 +11,22 @@ function inputFunction() {
   }
 }
 function displayArray() {
-  let p = document.getElementById("p1");
+  let p = document.getElementById("showValue");
   p.innerHTML = "";
-  inputArray.forEach((element) => {
-    p.innerHTML += `<p> <input type="checkbox"> ${element}
-  <span class="material-symbols-outlined deleteIcon" onclick="deleteFuntion(this)" >
+  inputArray.forEach((element, index) => {
+    p.innerHTML += `<p> <input type="checkbox" class='cbox' id='cbox-${index}'> ${element} 
+  <span class="material-symbols-outlined deleteIcon" onclick="deleteFuntion(${index})">
     delete
     </span></p>`;
   });
 }
 
-function deleteFuntion(element) {
-  element.parentElement.style.display = "none";
+function deleteFuntion(index) {
+  let cbox = document.getElementById(`cbox-${index}`);
+  if (cbox.checked) {
+    inputArray.splice(index, 1);
+    displayArray();
+  } else {
+        alert("Check the box");
+  }
 }
